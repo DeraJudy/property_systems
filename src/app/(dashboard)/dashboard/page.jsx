72aee1cd2746bdfc9eead19364692a2f2d7b51b5
Dera Judy <judithlemechi@gmail.com>
2026-02-28 22:02:20 +0100

    dashboard

--- src/app/(dashboard)/dashboard/page.jsx
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -2,9 +2,16 @@ import { Button } from "@/components/ui/button";
 
 export default function DashboardPage() {
   return (
-    <div>
-        <Button variants="destructive" />
-        Dashboard Page
+    <div className="space-y-6">
+      
+      <h1 className="text-2xl font-semibold">Dashboard</h1>
+
+      <div className="bg-white rounded-xl shadow-sm border p-6">
+        <p className="text-gray-600">
+          Dashboard Overview
+        </p>
+      </div>
+
     </div>
   );
 }
